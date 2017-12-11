70dd9192f4ddfd8c8f47aa6f2e2e0067efa575c5
Quynh0510 <tranquynh051097@gmail.com>
2017-12-11 20:34:24 +0700

    Initial commit

---
+++ Server/Server/CSDL.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class CSDL
+    {
+        public static string ketnoiCSDL()
+        {
+            return "Data Source=DESKTOP-R8K6UFP\\SQLEXPRESS;Initial Catalog=Chat;Integrated Security=True";
+        }
+
+    }
+}
